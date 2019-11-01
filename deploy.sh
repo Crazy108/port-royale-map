6c51674b6a99b58766bf08bd97c4eda98dd47901
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-01 13:47:25 +0000

    Deploy Port Royale 3 Offline Map to github.com/Crazy108/port-royale-map.git:gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-echo "DEPLOY FROM ${PWD} DIR"
+#!/bin/bash
+#set -x #debug mode
+if [[ -f ./.env ]]; then source ./.env; fi
+
+echo "DEPLOY FROM ROOT DIR"
 echo "GOOGLE MAP KEY IS ${GOOGLE_MAP_KEY}"
 echo "GOOGLE MAP KEY IS "${GOOGLE_MAP_KEY}
